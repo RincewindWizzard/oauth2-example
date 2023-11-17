e6378f63e389d8b16e1c05cf6a7cb6241e3e80f1
RincewindWizzard <git@magierdinge.de>
2023-11-17 16:19:28 +0100

    fo

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,54 @@
 import "bulma"
 import "./style/main.scss"
 
+const clientID = '893fc9cabde21ec35f77'
+const redirectURI = 'https://rincewindwizzard.github.io/oauth2-example/'
 const contentSection = document.getElementById('content')
+
+async function exchangeCodeForAccessToken(authorizationCode: string) {
+    const tokenEndpoint = 'https://github.com/login/oauth/access_token';
+
+    try {
+        const response = await fetch(tokenEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify({
+                client_id: clientID,
+                client_secret: 'DEIN_GITHUB_CLIENT_SECRET',
+                code: authorizationCode,
+                redirect_uri: redirectURI,
+            }),
+        });
+
+        const tokenData = await response.json();
+        console.log('Access Token:', tokenData.access_token);
+    } catch (error) {
+        console.error('Fehler beim Abrufen des Zugriffstokens:', error);
+    }
+}
 
 
 const urlParams = new URLSearchParams(window.location.search);
 const authorizationCode = urlParams.get('code');
-if(authorizationCode) {
-    if(contentSection) {
+if (authorizationCode) {
+    if (contentSection) {
         contentSection.innerHTML = `authorizationCode = ${authorizationCode}`
     }
+
+    exchangeCodeForAccessToken(authorizationCode).then(() => {
+        console.log('foo')
+    })
 } else {
     const token = localStorage.getItem('token')
-    if(!token) {
-        const clientID = '893fc9cabde21ec35f77'
-        const redirectURI = 'https://rincewindwizzard.github.io/oauth2-example/'
+    if (!token) {
+
         const authURL = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(redirectURI)}`
 
         console.log(authURL)
-        if(contentSection) {
+        if (contentSection) {
             contentSection.innerHTML = `<a href="${authURL}">Login</a>`
         }
     } else {
