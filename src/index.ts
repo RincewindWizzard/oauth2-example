34a6b3037d44ab7486da1f24d1219e7a64742c82
RincewindWizzard <git@magierdinge.de>
2023-11-17 15:59:37 +0100

    Authorization code

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,31 @@ import "./style/main.scss"
 
 const contentSection = document.getElementById('content')
 
-if(contentSection) {
-    contentSection.innerText = 'Hier kommt noch was!'
+
+const urlParams = new URLSearchParams(window.location.search);
+const authorizationCode = urlParams.get('code');
+if(authorizationCode) {
+    if(contentSection) {
+        contentSection.innerHTML = `authorizationCode = ${authorizationCode}`
+    }
+} else {
+    const token = localStorage.getItem('token')
+    if(!token) {
+        const clientID = '893fc9cabde21ec35f77'
+        const redirectURI = 'https://rincewindwizzard.github.io/oauth2-example/'
+        const authURL = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(redirectURI)}`
+
+        console.log(authURL)
+        if(contentSection) {
+            contentSection.innerHTML = `<a href="${authURL}">Login</a>`
+        }
+    } else {
+
+    }
 }
+
+
+
+
+
+
